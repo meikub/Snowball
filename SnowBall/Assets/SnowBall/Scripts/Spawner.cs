9c784886a126ccd7b2d15fcb6045e1b86032dab8
meikub <44545393+meikub@users.noreply.github.com>
2020-12-21 17:05:45 +0100

    initial commit
    Snowball Multiplayer Fight

---
+++ SnowBall/Assets/SnowBall/Scripts/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+namespace SnowBall
+{
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] GameObject playerPrefab, cameraPrefab, rightHandPrefab, vrRightHand, leftHandPrefab, vrLeftHand, torsoPrefab, vrTorso;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject spawnedPlayer = PhotonNetwork.Instantiate(playerPrefab.name, cameraPrefab.transform.position, Quaternion.identity);
+        GameObject spawnedLeftHand = PhotonNetwork.Instantiate(leftHandPrefab.name,vrLeftHand.transform.position,Quaternion.identity);
+        GameObject spawnedRightHand = PhotonNetwork.Instantiate(rightHandPrefab.name,vrRightHand.transform.position,Quaternion.identity);
+        GameObject spawnedTorso = PhotonNetwork.Instantiate(torsoPrefab.name,vrTorso.transform.position,Quaternion.identity);
+        spawnedPlayer.transform.parent = cameraPrefab.transform;
+        spawnedLeftHand.transform.parent = vrLeftHand.transform;
+        spawnedRightHand.transform.parent = vrRightHand.transform;
+        spawnedTorso.transform.parent = vrTorso.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+}
